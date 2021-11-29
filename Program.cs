f9fb37bbbaae4804fa189d7df5be5e9aac712308
Name <osmprobox@gmail.com>
2021-11-29 13:37:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SkillFactory.ColoringNumbers
+{ 
+    class Program
+    { 
+        static readonly Random random = new Random();   
+
+        public static void Main()
+        {
+            const string HelloWorldRus = "ПриветМир!!!";
+            const string HelloWorldEn = "ПриветМир!!!";
+            const string HelloWorldEs = "ПриветМир!!!";
+
+            var str = new[] {HelloWorldRus, HelloWorldEn, HelloWorldEs};
+            var list = Enum.GetValues(typeof(ConsoleColor)).Cast<ConsoleColor>.ToArray();
+
+            do
+            {
+                foreach (var strs in str)
+                {
+                    foreach (var word in strs)
+                    {
+                        var rnd = random.Next(list.Count());
+                        var color = list[rnd];
+
+                        if (color==ConsoleColor.Black)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            System.Diagnostics.Debug.WriteLine("Черный цвет сейчас был бы...");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = color;
+                            System.Diagnostics.Debug.WriteLine($"Сейчас цвет { color }");
+                        }
+                        Console.Write(word);
+                    }
+                }
+            }
+
+        }
+    }
+}
